40d65d50a2396f7ebee781dd60832b6f79701137
1412557 <kysydragony@gmail.com>
2017-10-06 17:25:01 +0700

    Add basic files from tutorial
    Install newtonsoft.json
    Install Monogame

---
+++ game/Unit.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    class Unit : VisibleEntity
+    {
+        public Texture2D Texture { get; set; }
+
+        public int Rows { get; set; }
+
+        public int Column { get; set; }
+
+        private int currentFrame;
+
+        private int totalFrame;
+
+        public MoveUnit(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Column = columns;
+            currentFrame = 0;
+            totalFrame = Rows * Column;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
